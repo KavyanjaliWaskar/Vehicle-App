973b0fabcb04fd2bfdb68d6a39b33ea3444c57da
Kavyanjali Waskar <kavyanjaliwaskar@gmail.com>
2026-09-07 17:40:56 +0530

    Fix database host

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST.trim(),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
