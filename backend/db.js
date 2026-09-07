2fde54b4890ec033fa10c642855ada117093b59c
Kavyanjali Waskar <kavyanjaliwaskar@gmail.com>
2026-09-07 17:48:28 +0530

    Fix Aiven SSL connection

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT || 3306),
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
